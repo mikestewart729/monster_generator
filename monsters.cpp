a649d30fda48467a8a638d5025b9c1c015ccd7fb
Mike <mikestewart729@gmail.com>
2025-04-14 13:27:49 -0400

    solve part d. add a constructor for Monster objects

--- monsters.cpp
+++ monsters.cpp
@@ -31,9 +31,24 @@ public:
         maxMonsterTypes,
     };
 
+    Monster(Type type, std::string_view name, std::string_view roar, int hit_points)
+        : m_type { type }
+        , m_name { name }
+        , m_roar { roar }
+        , m_hit_points { hit_points }
+    {
+    }
+
 private:
     Type m_type {};
     std::string m_name {""};
     std::string m_roar {""};
     int m_hit_points {};
 };
+
+int main() 
+{
+    Monster skeleton { Monster::skeleton, "Bones", "*rattle*", 4 };
+
+    return 0;
+}
